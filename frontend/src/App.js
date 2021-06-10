a2ff9ca8b94935e03977efefa7394f67ec42a78f
dbayandarov@gmail.com <dbayandarov@gmail.com>
2021-06-10 16:57:28 -0700

    revamped fronted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { SubmitForm } from "./components/submitForm.js";
 import {BackgroundImg} from "./components/backgroundImg.js"
-import {AppBar, Typography, makeStyles, Button, Link} from '@material-ui/core';
+import {AppBar, Typography, makeStyles, Button, Link, Container, Box, Grid, Avatar, Ca} from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
-
+import Album from "./components/Album.js"
 
 function App() {
     const useStyles = makeStyles(theme => ({
@@ -16,6 +16,12 @@ function App() {
       },
       menuItem: {
         marginRight: "5%"
+      },
+      containerGallery: {
+        margin: theme.spacing(2)
+      },
+      galleryHeader: {
+        fontWeight: 800
       }
     })); 
     const classes = useStyles();
@@ -25,30 +31,7 @@ function App() {
     }
     return (
       <div className="App">
-         {/* <SubmitForm
-              formName="Look-up a STONK"
-              // formDescription="This is sample form using Material UI."
-          /> */}
-          <AppBar className={classes.backgroundApp} position="static">
-            <Toolbar className={classes.toolBar}>
-              <Typography variant="h6" color="primary" className={classes.menuItem}>
-                <Link color="inherit" href="/" onClick={handleClick}>
-                  Home
-                </Link>
-              </Typography>
-              <Typography variant="h6" color="primary" className={classes.menuItem}>
-                <Link color="inherit" href="/" onClick={handleClick}>
-                  About
-                </Link>
-              </Typography>
-              <Typography variant="h6" color="primary">
-                <Link color="inherit" href="/" onClick={handleClick}>
-                  Login
-                </Link>
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <BackgroundImg/>
+          <Album/>
       </div>
   );
 }
---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link} from "@material-ui/core";
+
+
+export default function MenuNav() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton aria-controls="simple-menu" color="inherit" aria-haspopup="true" onClick={handleClick}>
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+            <Link color="inherit">Home </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+            <Link color="inherit">About </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+            <Link color="inherit">My Account </Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
